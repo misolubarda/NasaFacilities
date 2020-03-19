e5dbb3347e60f830f1b703d6fd5677d215b00db9
Miso Lubarda <miso.lubarda@gmail.com>
2020-03-19 16:22:36 +0100

    Initial Xcode project

---
+++ NasaFacilities/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NasaFacilities
+//
+//  Created by Miso Lubarda on 19.03.20.
+//  Copyright © 2020 Miso Lubarda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
